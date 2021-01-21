8417d8b9b512374ece5a7ad9e381995f2b2c4a20
Ruby <bazzani.joan@gmail.com>
2021-01-21 22:47:46 +0100

    adding commentarYYYYYYYY

--- lib/cher_deputes.rb
+++ lib/cher_deputes.rb
@@ -23,35 +23,30 @@ méthode = 1) on récupère les prénoms
 require "nokogiri"
 require "open-uri"
 
-def get_deputy_email(page)
-  deputy_href = page.css('td[1]/a/@href').text
-  deputy_url = Nokogiri::HTML(URI.open("http://www2.assemblee-nationale.fr#{deputy_href}"))
-  deputy_email = deputy_url.css('.deputes-liste-attributs > dd:nth-child(8) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)').text
+def get_deputy_email(page) # Prend les emails des députés / "page" = paramètre
+  deputy_href = page.css('td[1]/a/@href').text # Prend tous les href des différentes pages des députés et formatage .text  https://stackoverflow.com/questions/2505104/html-to-plain-text-with-ruby
+  deputy_url = Nokogiri::HTML(URI.open("http://www2.assemblee-nationale.fr#{deputy_href}")) # fabrique mes url pour scrapper mes données
+  deputy_email = deputy_url.css('.deputes-liste-attributs > dd:nth-child(8) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)').text 
+  # crée une variable avec le chemin pour scrapper les emails et formatage .text 
 end
   
-def get_deputy_first_name(first_name)
-  first_name = first_name.css('td[1]/a').text.delete_prefix('Mme ').delete_prefix('M, ').split.first
+def get_deputy_first_name(first_name) # méthode pour stocker les prénoms des députés
+  first_name = first_name.css('td[1]/a').text.delete_prefix('Mme ').delete_prefix('M. ').split.first  
+  # Ici on utilise delete_prefix pour enlever "M." et "Mme", .split.first pour découper la string par mots pour la stocker dans un array, 
+  # peut aussi s'utiliser avec split(//) pour découper par lettres, "first" prend la première valeur de l'array
 end
 
-def get_deputy_last_name(last_name)
-  last_name = last_name.css('td[1]/a').text.delete_prefix('Mme ').delete_prefix('M, ').split.last
+def get_deputy_last_name(last_name) # méthode pour stocker les noms des députés
+  last_name = last_name.css('td[1]/a').text.delete_prefix('Mme ').delete_prefix('M. ').split.last 
+  # Comme ci-dessus sauf que "last" prend la dernière valeur de l'array
 end
 
-def puts_deputy_on_array
+def puts_deputy_on_array # méthode pour mettre les données sur les députés dans des hash puis dans un tableau
   page = Nokogiri::HTML(URI.open('http://www2.assemblee-nationale.fr/deputes/liste/tableau'))
-  tab = page.css('//table/tbody/tr')
+  tab = page.css('//table/tbody/tr') # tab = site.css nokogiri pour scrapper le code html où sont stockées les données du tableau
   deputy_hash = tab.map {|x| {'first_name' => get_deputy_first_name(x), 'last_name' => get_deputy_last_name(x), 'email' => get_deputy_email(x) } }
+  # Prend les données du tableau, utilise ".map" pour créer un array avec ces données
 end
 
-begin
-    pp puts_deputy_on_array
-  rescue StandardError => e
-    puts 'You messed up in debuty'
-  end
-  begin
-    pp puts_deputy_on_array
-  rescue StandardError => e
-    puts e.message
-  else
-    puts 'Well Done!'
-  end
+#juste un puts
+puts puts_deputy_on_array
